3ee7f1265dd190ea8d7819dc6f5b7e01d3b362a1
Alexander Cook <awcook@mail.usf.edu>
2020-01-17 21:26:14 -0500

    rough draft of dfs function. removed new lines from adjacency list

--- dfs.py
+++ dfs.py
@@ -1,11 +1,11 @@
 def dfs(G, I, F):
-    list = [] #list to track order of visited nodes
+    pathMap = {} #map to track order of visited nodes
 
     visited = {} #map of vertex to bool set to false
     for vertex in G:
         visited[vertex] = False
 
-    stack = [] #stack tracks order of vertices to visit
+    stack = [] #stack tracks traversal order of vertices for the dfs
     stack.append(I) #push initial vertex
 
     while stack:
@@ -13,12 +13,13 @@ def dfs(G, I, F):
         if(visited[current]): continue #if node is already visited skip rest of loop
         visited[current] = True #set current node to visited
 
-        adjacents = G[current]
+        for neighbor in G[current]: #for all vertices adjacent to current vertex
+            stack.append(neighbor)
+            pathMap[neighbor] = current #update the current path from the starting point
 
-        for vertex in adjacents:
-            stack.append(vertex)
+        if current == F: return pathMap #once final vertex  is found return the path
 
-
+    return False #final vertex was not found
 
 
 
@@ -29,8 +30,9 @@ with open("connections.txt", 'r') as f:
         if 'END' in line:
           break
 
+        line = line.strip('\n') #remove newline
         li = list(line.split(" ")) #convert each line of the file to a list
-        adjList[li[0]] = li[2:] #map the vertex to its adjacent vertices
+        adjList[li[0]] = sorted(li[2:]) #map the vertex to its adjacent vertices
 
        
 #print dictionary for testing
@@ -38,8 +40,8 @@ for key in adjList.keys():
     print(key, ':', adjList[key])
 
 
-I = input("Enter the initial vertex\n")
-F = input("Enter the final vertex\n")
-print("The initial vertex is: " + I + " and the final vertex is: " + F)
+#I = input("Enter the initial vertex\n")
+#F = input("Enter the final vertex\n")
+#print("The initial vertex is: " + I + " and the final vertex is: " + F)
 
 #dfs(adjList, I, F)
