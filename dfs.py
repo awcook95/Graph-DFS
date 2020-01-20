2378c2cd517f6e485533ab6e3172fc227ece1467
Alexander Cook <awcook@mail.usf.edu>
2020-01-20 11:47:18 -0500

    Working input and output. There is an issue with updating the path during the dfs

--- dfs.py
+++ dfs.py
@@ -10,14 +10,14 @@ def dfs(G, I, F):
 
     while stack:
         current = stack.pop()
+        if current == F: return pathMap #once final vertex  is found return the path
+
         if(visited[current]): continue #if node is already visited skip rest of loop
         visited[current] = True #set current node to visited
 
         for neighbor in G[current]: #for all vertices adjacent to current vertex
             stack.append(neighbor)
             pathMap[neighbor] = current #update the current path from the starting point
-
-        if current == F: return pathMap #once final vertex  is found return the path
 
     return False #final vertex was not found
 
@@ -36,12 +36,28 @@ with open("connections.txt", 'r') as f:
 
        
 #print dictionary for testing
-for key in adjList.keys():
+for key in adjList:
     print(key, ':', adjList[key])
 
 
 #I = input("Enter the initial vertex\n")
 #F = input("Enter the final vertex\n")
-#print("The initial vertex is: " + I + " and the final vertex is: " + F)
+I = "A1"
+F = "G1"
 
-#dfs(adjList, I, F)
+print("The initial vertex is: " + I + " and the final vertex is: " + F)
+
+pathMap = dfs(adjList, I, F)
+
+print("The path map: ")
+for vertex in pathMap:
+    print(vertex, ':', pathMap[vertex])
+
+"""
+if pathMap != False: #if dfs succeeded
+    curr = F #start at the target node
+    while (curr != None):
+        print(curr)
+        curr = pathMap[curr] #and use the path map to trace back the steps taken
+else: print("A path to the final vertex was not found")
+"""
